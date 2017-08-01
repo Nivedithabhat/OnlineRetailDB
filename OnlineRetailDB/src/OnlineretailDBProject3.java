cc2e3076c4d04a24c1bb4f974b3a7509846a7642
SamhithBharadwaj <samhith.bharadwaj@syncron.com>
2017-08-01 15:47:51 +0530

    sql scripts required

--- OnlineRetailDB/src/OnlineretailDBProject3.java
+++ OnlineRetailDB/src/OnlineretailDBProject3.java
@@ -1,5 +1,6 @@
 import com.sam.ui.Main;
 
+/*Run sql script in the file SQLscript in your database console to add required tables*/
 public class OnlineretailDBProject3 {
 	public static void main(String[] args) {
 		new Main().start();
